9490ad3d055ad614b0ec1229bd22b10b9143faab
Oliver Caldwell <ollie@oli.me.uk>
2019-12-28 16:22:04 +0000

    Have code evaluating and results parsed
    All connections etc are hard coded right now. Paves the way for
    everything else near enough.

--- fnl/conjure-sourcery/main.fnl
+++ fnl/conjure-sourcery/main.fnl
@@ -1,6 +1,33 @@
+(local ani (require :aniseed.core))
+(local nvim (require :aniseed.nvim))
+(local nu (require :aniseed.nvim.util))
+
 (fn main []
-  ;; (print "Sourcery!?")
-  )
+  (let [chan-id (nvim.fn.sockconnect
+                  :tcp "localhost:5555"
+                  {:on_data :ConjureSourceryChanOnData})]
+    (nvim.fn.chansend chan-id "(+ 10 20)\n"))
+  nil)
+
+(fn parse [s]
+  {:tag (: (: s :match ":tag :%a+") :sub 7)
+   :val (: (: s :match ":val %b\"\"") :sub 7 -2)})
+
+(fn chan-on-data [chan-id data]
+  (->> data
+      (ani.first)
+      (parse)
+      (ani.pr "resp=>"))
+  (nvim.fn.chanclose chan-id)
+  nil)
+
+(nu.fn-bridge :ConjureSourceryChanOnData
+              :conjure-sourcery.main
+              :chan-on-data)
+
+(comment
+  ((. (require :conjure-sourcery.main) :main)))
 
 {:aniseed/module :conjure-sourcery.main
- :main main}
+ :main main
+ :chan-on-data chan-on-data}
--- lua/conjure-sourcery/main.lua
+++ lua/conjure-sourcery/main.lua
@@ -1,4 +1,21 @@
+local ani = require("aniseed.core")
+local nvim = require("aniseed.nvim")
+local nu = require("aniseed.nvim.util")
 local function main()
-  return print("Sourcery!?")
+  do
+    local chan_id = nvim.fn.sockconnect("tcp", "localhost:5555", {on_data = "ConjureSourceryChanOnData"})
+    nvim.fn.chansend(chan_id, "(+ 10 20)\n")
+  end
+  return nil
 end
-return {["aniseed/module"] = "conjure-sourcery.main", main = main}
+local function parse(s)
+  return {tag = s:match(":tag :%a+"):sub(7), val = s:match(":val %b\"\""):sub(7, -2)}
+end
+local function chan_on_data(chan_id, data)
+  ani.pr("resp=>", parse(ani.first(data)))
+  nvim.fn.chanclose(chan_id)
+  return nil
+end
+nu["fn-bridge"]("ConjureSourceryChanOnData", "conjure-sourcery.main", "chan-on-data")
+              -- ((. (require conjure-sourcery.main) main))
+return {["aniseed/module"] = "conjure-sourcery.main", ["chan-on-data"] = chan_on_data, main = main}
