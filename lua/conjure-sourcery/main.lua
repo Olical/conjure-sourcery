4f04145ebfa6ccf19768d48fcdebc37eea56e30a
Oliver Caldwell <ollie@oli.me.uk>
2019-12-29 16:02:47 +0000

    Use propel to start the prepl and write the file

--- lua/conjure-sourcery/main.lua
+++ lua/conjure-sourcery/main.lua
@@ -1,21 +1,24 @@
+              -- ((. (require conjure-sourcery.main) single-eval) (+ 10 20) )
 local ani = require("aniseed.core")
 local nvim = require("aniseed.nvim")
 local nu = require("aniseed.nvim.util")
-local function main()
-  do
-    local chan_id = nvim.fn.sockconnect("tcp", "localhost:5555", {on_data = "ConjureSourceryChanOnData"})
-    nvim.fn.chansend(chan_id, "(+ 10 20)\n")
-  end
-  return nil
-end
 local function parse(s)
   return {tag = s:match(":tag :%a+"):sub(7), val = s:match(":val %b\"\""):sub(7, -2)}
 end
 local function chan_on_data(chan_id, data)
-  ani.pr("resp=>", parse(ani.first(data)))
+  ani.pr(parse(ani.first(data)))
   nvim.fn.chanclose(chan_id)
   return nil
 end
 nu["fn-bridge"]("ConjureSourceryChanOnData", "conjure-sourcery.main", "chan-on-data")
-              -- ((. (require conjure-sourcery.main) main))
-return {["aniseed/module"] = "conjure-sourcery.main", ["chan-on-data"] = chan_on_data, main = main}
+local function main()
+  return ani.pr("Sourcery!?")
+end
+local function single_eval(code)
+  do
+    local chan_id = nvim.fn.sockconnect("tcp", ("localhost:" .. ani.first(nvim.fn.readfile(".prepl-port"))), {on_data = "ConjureSourceryChanOnData"})
+    nvim.fn.chansend(chan_id, code)
+  end
+  return nil
+end
+return {["aniseed/module"] = "conjure-sourcery.main", ["chan-on-data"] = chan_on_data, ["single-eval"] = single_eval, main = main}
